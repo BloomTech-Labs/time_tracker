8b89efe8eb828c3ce15c52ac2e353aea44c21865
Troy Williams <troyDonwilliams@gmail.com>
2018-05-21 12:00:42 -0500

    setup folder structure for components. connecting to mlab

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const vendorRouter = require('./vendor/vendorRoutes');
 
 const server = express();
+server.use('/vendor', vendorRouter);
 
-server.listen(5000, () => {
-	console.log('Server running on port 5000');
-});
+mongoose
+  .connect('mongodb://admin:temp@ds231090.mlab.com:31090/labs_time_tracker')
+  .then(success => {
+    server.listen(5000, () => {
+      console.log('Server running on port 5000');
+    });
+  })
+  .catch(err => {
+    console.error('ERROR CONNECTING TO MLAB');
+  });
