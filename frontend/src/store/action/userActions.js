8a2d67d367e2e32f412549668d8943db457dd0c3
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-05-22 12:39:38 -0400

    Merged frontend with frontend_prod, links for Heroku

--- frontend/src/store/action/userActions.js
+++ frontend/src/store/action/userActions.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 
 export const signUp = ({ name, email, password, type }) => {
   return dispatch => {
@@ -17,6 +18,30 @@ export const signUp = ({ name, email, password, type }) => {
         .post('/vendor', { name, email, password })
         .then(({ data }) => {
           dispatch({ type: SIGNUP, payload: data });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+};
+
+export const logIn = ({ email, password, type }) => {
+  return dispatch => {
+    if (type === 'client') {
+      axios
+        .post('/client/login', { email, password })
+        .then(({ data }) => {
+          dispatch({ type: LOGIN, payload: data });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } else {
+      axios
+        .post('/vendor/login', { email, password })
+        .then(({ data }) => {
+          dispatch({ type: LOGIN, payload: data });
         })
         .catch(err => {
           console.log(err);
