6f62e3316823250932bca589c4f1481a24ad82ae
Troy Williams <troyDonwilliams@gmail.com>
2018-06-04 17:39:19 -0500

    add signup button on login page

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -32,61 +32,73 @@ class Login extends Component {
 
   render() {
     return (
-      <Row>
-        <Col md="4" />
-        <Col>
-          <Form onSubmit={e => this.onSubmitHandler(e)}>
-            <FormGroup>
-              <Label for="email">Email</Label>
-              <Input
-                type="email"
-                name="email"
-                id="email"
-                value={this.state.email}
-                placeholder="with a placeholder"
-                onChange={this.inputChangeHandler}
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="password">Password</Label>
-              <Input
-                type="password"
-                name="password"
-                id="password"
-                value={this.state.password}
-                placeholder="password placeholder"
-                onChange={this.inputChangeHandler}
-              />
-            </FormGroup>
-            <FormGroup tag="fieldset">
-              <legend>Vendor or Client</legend>
-              <FormGroup check>
-                <Label check>
-                  <Input
-                    type="radio"
-                    name="type"
-                    value="vendor"
-                    onChange={this.inputChangeHandler}
-                    defaultChecked
-                  />Vendor
-                </Label>
+      <div>
+        <Row>
+          <Col md="4" />
+          <Col>
+            <Form onSubmit={e => this.onSubmitHandler(e)}>
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={this.state.email}
+                  placeholder="with a placeholder"
+                  onChange={this.inputChangeHandler}
+                />
               </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input
-                    type="radio"
-                    name="type"
-                    value="client"
-                    onChange={this.inputChangeHandler}
-                  />Client
-                </Label>
+              <FormGroup>
+                <Label for="password">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={this.state.password}
+                  placeholder="password placeholder"
+                  onChange={this.inputChangeHandler}
+                />
               </FormGroup>
-            </FormGroup>
-            <Button>Submit</Button>
-          </Form>
-        </Col>
-        <Col md="4" />
-      </Row>
+              <FormGroup tag="fieldset">
+                <legend>Vendor or Client</legend>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="radio"
+                      name="type"
+                      value="vendor"
+                      onChange={this.inputChangeHandler}
+                      defaultChecked
+                    />Vendor
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="radio"
+                      name="type"
+                      value="client"
+                      onChange={this.inputChangeHandler}
+                    />Client
+                  </Label>
+                </FormGroup>
+              </FormGroup>
+              <Button>Submit</Button>
+            </Form>
+          </Col>
+          <Col md="4" />
+        </Row>
+        <Row>
+          <Col md="4" />
+          <Col>
+            <div>Don't have an account?</div>
+            <Button onClick={() => this.props.history.push('/signup')}>
+              Signup
+            </Button>
+          </Col>
+          <Col md="4" />
+        </Row>
+      </div>
     );
   }
 }
